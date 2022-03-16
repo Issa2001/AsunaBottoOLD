2311d55c2bc8ea1e17af99140d5843ab1567544e
İşşa <Issamoha020@gmail.com>
2022-03-16 08:08:29 +0300

    Add files via upload

---
+++ src/commands/Dev/promoteme.ts
@@ -0,0 +1,33 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            modsOnly: true,
+            command: 'promoteme',
+            description: 'promotes bot owner & mods',
+            category: 'dev',
+            usage: `${client.config.prefix}promoteme [@mention | tag]`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
+            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
+        M.mentioned.forEach(async (user) => {
+            const usr = this.client.contacts[user]
+            const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
+            if (M.groupMetadata?.admins?.includes(user)) M.reply(`❌ Skipped *${username}* as they're already an admin`)
+            else {
+                await this.client.groupMakeAdmin(M.from, [user])
+                M.reply(`*🚥Status:*\n\n✅Promoted *${username}*`)
+            }
+        })
+    }
+}
