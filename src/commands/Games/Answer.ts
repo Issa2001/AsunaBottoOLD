c4059f1b9ebfd04a176cb943a6a672bf0e7faa4e
İşşa <Issamoha020@gmail.com>
2022-04-07 13:11:14 +0300

    Add files via upload

--- src/commands/Games/Answer.ts
+++ src/commands/Games/Answer.ts
@@ -18,7 +18,7 @@ export default class Command extends BaseCommand {
     M: ISimplifiedMessage,
     { joined }: IParsedArgs
   ): Promise<void> => {
-    if (M.from !== "120363041058221000@g.us")
+    if (M.from !== "120363040743836704@g.us")
       return void M.reply(
         `You can't use this command here. Use ${this.client.config.prefix}support to get the quiz group link.`
       );
