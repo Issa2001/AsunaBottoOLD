78b6a62971b05ddee738c4837cf811463e788a6e
İşşa <Issamoha020@gmail.com>
2022-04-17 11:16:18 +0300

    Add files via upload

--- src/commands/General/support.ts
+++ src/commands/General/support.ts
@@ -20,11 +20,11 @@ export default class Command extends BaseCommand {
         (await this.client.sendMessage(
         M.sender.jid,
                 `*📮𝗦𝘂𝗽𝗽𝗼𝗿𝘁 𝗚𝗿𝗼𝘂𝗽 𝗟𝗶𝗻𝗸𝘀*
-「ֆAƤƤꫝɨ℟E: RE🈲」:\n\nhttps://chat.whatsapp.com/DBkC5mV10TkCQUhc753kyb\n\n「𝗦𝗮𝗽𝗽𝗵𝗶𝗿𝗲: 𝗖𝗮𝘀𝗶𝗻𝗼💰」:\n\nhttps://chat.whatsapp.com/BAHvPJ05TNo4yvgc8wyJ1r\n\n 「𝗦𝗮𝗽𝗽𝗵𝗶𝗿𝗲: 𝙌𝙪𝙞𝙯📑」:\n\nhttps://chat.whatsapp.com/EtPNYEtYSqwJTsL5vshDlH\n\n 「𝗦𝗮𝗽𝗽𝗵𝗶𝗿𝗲: 𝗡𝘀𝗳𝘄🧜‍♀️」: \n\nhttps://chat.whatsapp.com/FjMlLFPTBPDLutAKqvcowM`,
+「ֆAƤƤꫝɨ℟E: RE🈲」:\n\nhttps://chat.whatsapp.com/DBkC5mV10TkCQUhc753kyb\n\n「𝗦𝗮𝗽𝗽𝗵𝗶𝗿𝗲: 𝗖𝗮𝘀𝗶𝗻𝗼💰」:\n\nhttps://chat.whatsapp.com/BAHvPJ05TNo4yvgc8wyJ1r\n\n 「𝗦𝗮𝗽𝗽𝗵𝗶𝗿𝗲: 𝙌𝙪𝙞𝙯📑」:\n\nhttps://chat.whatsapp.com/EtPNYEtYSqwJTsL5vshDlH`,
            MessageType.text
         ))
         const n = [
-            './assets/images/asuna-yuuki.png','./assets/images/yuuki-asuna.jpeg','./assets/images/Asuna-logo.jpg',
+            './assets/images/asuna-yuuki.png,./assets/images/yuuki-asuna.jpeg'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
