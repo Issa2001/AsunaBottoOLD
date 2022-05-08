26c07966cfa947d9238b623a825922eb2bd17b6f
İşşa <Issamoha020@gmail.com>
2022-05-08 11:57:02 +0300

    Add files via upload

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -1,30 +1,33 @@
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'mods',
-            description: "Displays the Moderators' contact info",
-            category: 'general',
-            usage: `${client.config.prefix}mods`,
-            aliases: ['moderators', 'dev', 'mod', 'owner']
-        })
-    }
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "mods",
+      description: "Displays the Moderators' contact info",
+      category: "general",
+      usage: `${client.config.prefix}mods`,
+      aliases: ["moderators", "dev", "mods", "owner"],
+    });
+  }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        if (!this.client.config.mods || !this.client.config.mods[0]) return void M.reply('*[UNMODERATED]*')
-        const filteredMap = this.client.config.mods.map((mod) => this.client.getContact(mod)).filter((user) => user)
-        let text = '*🏅𝖣𝖾𝗏𝖾𝗅𝗈𝗉𝖾𝗋 & 𝖬𝗈𝖽𝖾𝗋𝖺𝗍𝗈𝗋𝗌🎖️*\n\n'
-        filteredMap.forEach(
-            (user, index) =>
-                (text += `#${index + 1}\n🎫 *Username: ${
-                    user.notify || user.vname || user.name || 'null'
-                }*\n📮 *Contact: https://wa.me/+${user?.jid?.split('@')[0]}*\n\n`) 
-        )
-        text += `\n© 𝖠𝗌𝗎𝗇𝖺 2022`
-        return void M.reply(text)
-    }
+  run = async (M: ISimplifiedMessage): Promise<void> => {
+    if (!this.client.config.mods || !this.client.config.mods[0])
+      return void M.reply("*[UNMODERATED]*");
+    const filteredMap = this.client.config.mods
+      .map((mod) => this.client.getContact(mod))
+      .filter((user) => user);
+    let text = "🧧「ASUNA MODS」🧧\n\n";
+    filteredMap.forEach(
+      (user, index) =>
+        (text += `#${index + 1}\n🎋 *Username: ${
+          user.notify || user.vname || user.name || "null"
+        }*\n📮 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
+    );
+    text += `\n© 𝖠𝗌𝗎𝗇𝖺 2022`;
+    return void M.reply(text);
+  };
 }
