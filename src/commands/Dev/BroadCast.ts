ba0207f8bb676910809c88fb5c31dd42eb93173c
İşşa <Issamoha020@gmail.com>
2022-05-10 09:53:07 +0300

    Add files via upload

--- src/commands/Dev/BroadCast.ts
+++ src/commands/Dev/BroadCast.ts
@@ -1,29 +1,52 @@
-import { MessageType } from '@adiwajshing/baileys'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import {  IParsedArgs, ISimplifiedMessage } from '../../typings'
+/** @format */
+
+import { MessageType } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { IParsedArgs, ISimplifiedMessage } from "../../typings";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'broadcast',
-            description: 'Sends msg to all group chats',
-            aliases: ['BC', 'announcement','bc'],
-            category: 'dev',
-            usage: `${client.config.prefix}broadcast`,
-            modsOnly: true,
-            baseXp: 0
-        })
-    }
+  constructor(client: WAClient, handler: MessageHandler) {
+    super(client, handler, {
+      command: "broadcast",
+      description:
+        "Will make a broadcast for groups where the bot is in. Can be used to make announcements.",
+      aliases: ["bcast", "announcement", "bc"],
+      category: "dev",
+      dm: true,
+      usage: `${client.config.prefix}bc`,
+      modsOnly: true,
+      baseXp: 0,
+    });
+  }
 
-    run = async (M: ISimplifiedMessage,  { joined }: IParsedArgs): Promise<void> => {
-        
-        const term = joined.trim();
-        const chats:any= this.client.chats.all().filter(v => !v.read_only && !v.archive).map(v => v.jid).map(jids => jids.includes("g.us")? jids : null).filter(v=>v);
-        for(let i =0;i<chats.length;i++){
-        const text = `*「 𝖠𝗌𝗎𝗇𝖺🚀 𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍 」* \n 📪𝗠𝗲𝘀𝘀𝗮𝗴𝗲: \n${term} \n 🪶𝗔𝘂𝘁𝗵𝗼𝗿: ${M.sender.username}`
-        this.client.sendMessage(chats[i], text,MessageType.text,{contextInfo : {mentionedJid : M.groupMetadata?.participants.map((user) => user.jid) }})
-        }
+  run = async (
+    M: ISimplifiedMessage,
+    { joined }: IParsedArgs
+  ): Promise<void> => {
+    if (!joined)
+      return void (await M.reply(`Please provide the Broadcast Message.`));
+    const term = joined.trim();
+    const images = [
+      "https://www.linkpicture.com/q/Asunabotto.jpg",
+    ];
+    const selected = images[Math.floor(Math.random() * images.length)];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const chats: any = this.client.chats
+      .all()
+      .filter((v) => !v.read_only && !v.archive)
+      .map((v) => v.jid)
+      .map((jids) => (jids.includes("g.us") ? jids : null))
+      .filter((v) => v);
+    for (let i = 0; i < chats.length; i++) {
+      const text = `*「𝖠𝗌𝗎𝗇𝖺🚀𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍」*\n\n${term}\n\n*🪶Author: ${M.sender.username}*`;
+      this.client.sendMessage(chats[i], { url: selected }, MessageType.image, {
+        caption: `${text}`,
+        contextInfo: {	mentionedJid: M.groupMetadata?.participants.map((user) => user.jid),
+				},
+      });
     }
+    await M.reply(`✅ Broadcast Message sent to *${chats.length} groups*.`);
+  };
 }
