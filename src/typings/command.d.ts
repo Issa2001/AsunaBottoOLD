3e75318b14f69eff291679c1dc13719ca1d71020
İşşa <Issamoha020@gmail.com>
2022-05-31 07:45:55 +0300

    Create command.d.ts

---
+++ src/typings/command.d.ts
@@ -0,0 +1,77 @@
+import MessageHandler from "../Handlers/MessageHandler";
+
+import { WAClient } from "../lib/WAClient";
+
+import { IParsedArgs, ISimplifiedMessage } from "./";
+
+export interface ICommand {
+
+  client?: WAClient;
+
+  handler?: MessageHandler;
+
+  run(
+
+    M: ISimplifiedMessage,
+
+    parsedArgs: IParsedArgs
+
+  ): Promise<void | never> | void | never;
+
+  config: {
+
+    adminOnly?: boolean;
+
+    aliases?: string[];
+
+    description?: string;
+
+    command: string;
+
+    id?: string;
+
+    category?: TCategory;
+
+    usage?: string;
+
+    dm?: boolean;
+
+    baseXp?: number;
+
+    modsOnly?: boolean;
+
+  };
+
+}
+
+export type TCategory =
+
+  | "characters"
+
+  | "coding"
+
+  | "dev"
+
+  | "economy"
+
+  | "fun"
+
+  | "games"
+
+  | "educative"
+
+  | "general"
+
+  | "media"
+
+  | "moderation"
+
+  | "pokemon"
+
+  | "nsfw"
+
+  | "utils"
+
+  | "weeb"
+
+  | "category";
